d2544036a39af3f8c93debedd37581ad8d2ab2e4
Alexey Eberil <aeberil@pawlin.ru>
2025-03-18 22:05:22 +0300

    Splitted into library and application

---
+++ env.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./build/.packages/o/osqp/latest/4e0143c97b65425b855ad5fd03038b6a/lib
--- xmake.lua
+++ xmake.lua
@@ -2,38 +2,19 @@ add_rules("plugin.compile_commands.autoupdate", { outputdir = "./build" })
 add_rules("utils.install.cmake_importfiles")
 add_rules("mode.debug", "mode.release")
 
-package("osqp")
-set_sourcedir(path.join(os.scriptdir(), "thirdparty/osqp/"))
-on_install(function(package)
-  local configs = {}
-  table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
-  table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-  import("package.tools.cmake").install(package, configs)
-end)
-on_test(function(package)
-  assert(package:has_cfuncs("csc_matrix", { includes = "osqp/cs.h" }))
-end)
-package_end()
-add_requires("osqp")
+includes("./heatsolver")
 
-add_requires("abseil")
-add_requires("fmt")
-add_requires("eigen")
 add_requires("conan::highfive/2.10.0", { alias = "highfive" })
 
-target("heatsolver")
+target("application")
 do
   set_kind("binary")
-  --set_kind("$(kind)")
 
-  add_headerfiles("./include/(**.h)", { install = true, prefixdir = "" })
-  add_includedirs("./include/", { public = true })
-  add_files("./src/*.cpp")
+  add_headerfiles("./app/include/(**.h)", { install = true, prefixdir = "" })
+  add_includedirs("./app/include/", { public = true })
+  add_files("./app/src/*.cpp")
 
-  add_headerfiles("./thirdparty/osqp-cpp/include/(**.h)", { install = true, prefixdir = "" })
-  add_includedirs("./thirdparty/osqp-cpp/include/", { public = true })
-  add_files("./thirdparty/osqp-cpp/src/osqp++.cc")
-
-  add_packages("highfive", "fmt", "osqp", "abseil", "eigen")
+  add_deps("heatsolver")
+  add_packages("highfive")
 end
 target_end()
